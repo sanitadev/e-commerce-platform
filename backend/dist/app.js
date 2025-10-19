c724bd23169b8e9d6c3581d98ffe726d7d2fae65
sanitadev <colpasanita@gmail.com>
2025-10-19 15:29:05 +0200

    removing previous files and setting up a backend for this project

---
+++ backend/dist/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.get("/", (_req, res) => res.send("API running..."));
+export default app;
+//# sourceMappingURL=app.js.map
--- backend/routes/payment.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPayment, executePayment } = require('../controllers/payment.controller');
-const { protect } = require('../middleware/auth.middleware');
-
-router.post('/create', protect, createPayment);
-router.post('/execute', protect, executePayment);
-
-module.exports = router;
